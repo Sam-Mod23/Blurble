3dae3990426a2173839793fd691a5f6125d7eace
Nick Benson <nk.benson@hotmail.com>
2021-04-22 17:08:20 +0100

    Render searchBar Search bar was working with a string literal (search), and fetch is working. Current error is coming from the data.map, where data is an empty array. Last change was to move the extracted BookList component (now commented out) into the return block of BookSubmitScreen.

--- BlurbleApp/Screens/BookSubmit.jsx
+++ BlurbleApp/Screens/BookSubmit.jsx
@@ -1,91 +1,52 @@
 import React, { useEffect, useState } from "react";
-import {
-  FlatList,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  StatusBar,
-} from "react-native";
+import { View } from "react-native";
+import { SearchBar, ListItem } from "react-native-elements";
 
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress}>
-    <Text>{item.id}</Text>
-  </TouchableOpacity>
-);
+// const BookList = (props) => {
+//   console.log(props, "<<< props");
+//   const { data } = props;
+//   return data.map((item) => {
+//     <ListItem key={item.id} bottomDivider>
+//       <ListItem.Content>
+//         <ListItem.Title>{item.volumeInfo.title}</ListItem.Title>
+//         <ListItem.Subtitle>{item.volumeInfo.subtitle}</ListItem.Subtitle>
+//       </ListItem.Content>
+//     </ListItem>;
+//   });
+// };
 
-function BookSubmitScreen(props) {
-  const [data, setData] = useState();
-  const [selectedId, setSelectedId] = useState(null);
+const BookSubmitScreen = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=lord of the rings`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=the+lord+of+the+rings`)
       .then((response) => response.json())
       .then((json) => {
         setData(json.items);
       });
   }, []);
 
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-    const color = item.id === selectedId ? "white" : "black";
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
+    <View>
+      {console.log(data, "<<< data")}
+      <SearchBar
+        placeholder={"Search for a book"}
+        onChangeText={setSearch}
+        value={search}
+        onSubmit
       />
-    </SafeAreaView>
+      {/* <BookList data={data} /> */}
+      {data.map((item) => {
+        <ListItem key={item.id} bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title>{item.volumeInfo.title}</ListItem.Title>
+            <ListItem.Subtitle>{item.volumeInfo.subtitle}</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>;
+      })}
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
-
-/* const renderItem = ({item}) => {<Item title={item.title}/>
-  return (
-    <>
-      <View
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      ></View>
-      <ScrollView
-        style={{ flex: 5, justifyContent: "center", alignItems: "center" }}
-      >
-        <FlatList
-          data={data}
-          keyExtractor={(item) => {
-            item.title;
-          }}
-        />
-      </ScrollView>
-    </>
-  );
-}
-*/
+};
 
 export default BookSubmitScreen;
