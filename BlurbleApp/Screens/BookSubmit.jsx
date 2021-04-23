adba76255241eb20745bc955fb486493c1e4b198
Nick Benson <nk.benson@hotmail.com>
2021-04-23 10:22:24 +0100

    implement searchBar and scrollable list

--- BlurbleApp/Screens/BookSubmit.jsx
+++ BlurbleApp/Screens/BookSubmit.jsx
@@ -1,9 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { SearchBar, ListItem } from "react-native-elements";
 
+const BookSubmitScreen = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (search.length < 1) {
+      return;
+    } else {
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+        .then((response) => response.json())
+        .then((json) => setData(json.items));
+    }
+  }, [search]);
+
+  return (
+    <View>
+      <ScrollView>
+        <SearchBar
+          placeholder={"Search for a book"}
+          onChangeText={setSearch}
+          value={search}
+        />
+        {data.map((item) => {
+          return (
+            <ListItem key={item.id} bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{item.volumeInfo.title}</ListItem.Title>
+                <ListItem.Subtitle>
+                  {item.volumeInfo.subtitle}
+                </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
 // const BookList = (props) => {
-//   console.log(props, "<<< props");
 //   const { data } = props;
 //   return data.map((item) => {
 //     <ListItem key={item.id} bottomDivider>
@@ -14,39 +52,5 @@ import { SearchBar, ListItem } from "react-native-elements";
 //     </ListItem>;
 //   });
 // };
-
-const BookSubmitScreen = () => {
-  const [search, setSearch] = useState("");
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=the+lord+of+the+rings`)
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json.items);
-      });
-  }, []);
-
-  return (
-    <View>
-      {console.log(data, "<<< data")}
-      <SearchBar
-        placeholder={"Search for a book"}
-        onChangeText={setSearch}
-        value={search}
-        onSubmit
-      />
-      {/* <BookList data={data} /> */}
-      {data.map((item) => {
-        <ListItem key={item.id} bottomDivider>
-          <ListItem.Content>
-            <ListItem.Title>{item.volumeInfo.title}</ListItem.Title>
-            <ListItem.Subtitle>{item.volumeInfo.subtitle}</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>;
-      })}
-    </View>
-  );
-};
 
 export default BookSubmitScreen;
